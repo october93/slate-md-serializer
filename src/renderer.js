84019756269b74a846621fd787f3f1be5f9753cd
Juan Je Garcia <juanjegarcialopez@gmail.com>
2018-09-21 14:29:09 -0500

    OCT-2861 Fix at sign scaping

--- src/renderer.js
+++ src/renderer.js
@@ -151,6 +151,8 @@ const RULES = [
   {
     serialize(obj, children) {
       if (obj.object !== "mark") return;
+      if (!children) return;
+
       switch (obj.type) {
         case "bold":
           return `**${children}**`;
@@ -255,7 +257,7 @@ class Markdown {
     if (escape) {
       // escape markdown characters
       leavesText = leavesText
-        .replace(/([\\`*{}\[\]()#+\-.!_>@%])/gi, "\\$1")
+        .replace(/([\\`*{}\[\]()#+\-.!_>%])/gi, "\\$1")
         .replace(/\n/g, "  \n"); // format softBreaks
     }
     const string = new String({ text: leavesText });
